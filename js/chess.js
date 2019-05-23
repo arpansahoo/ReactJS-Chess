5d4e03d9ab4a6f9a1b258e2dd11d5a22b144dcdd
Arpan Sahoo <arpansahooedu@gmail.com>
2019-05-23 19:51:12 -0400

    React

---
+++ js/chess.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'index.css';
+import Game from './components/game.js'
+
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
