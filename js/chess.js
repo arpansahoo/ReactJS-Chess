5164e5c4ad3b13632ecd341223ee87d7bf41c55f
Arpan Sahoo <arpansahooedu@gmail.com>
2019-05-24 11:01:19 -0400

    initialize chess board

--- js/chess.js
+++ js/chess.js
@@ -1,143 +1,162 @@
-class Board extends React.Component {
-    renderSquare(i) {
-        var white_square = (i == 'A8' || i == 'C8' || i == 'E8' || i == 'G8'
-            || i == 'B7' || i == 'D7' || i == 'F7' || i == 'H7'
-            || i == 'A6' || i == 'C6' || i == 'E6' || i == 'G6'
-            || i == 'B5' || i == 'D5' || i == 'F5' || i == 'H5'
-            || i == 'A4' || i == 'C4' || i == 'E4' || i == 'G4'
-            || i == 'B3' || i == 'D3' || i == 'F3' || i == 'H3'
-            || i == 'A2' || i == 'C2' || i == 'E2' || i == 'G2'
-            || i == 'B1' || i == 'D1' || i == 'F1' || i == 'H1');
-        if (i == 'A8') { //top-left
-            return (
-                <div className="square white_square top_left_square"></div>
-            );
-        } else if (i == 'H8') { //top-right
-            return (
-                <div className="square black_square top_right_square"></div>
-            );
-        } else if (i == 'A1') { //bottom-left
-            return (
-                <div className="square black_square bottom_left_square"></div>
-            );
-        } else if (i == 'H1') { //bottom-right
-            return (
-                <div className="square white_square bottom_right_square"></div>
-            );
-        }
+function Square(props) {
+    return (
+        <button className = {"square " + props.color + props.corner}
+        onClick = {props.onClick}>
+            {props.value}
+        </button>
+    );
+}
 
-        if (white_square == true) {
-            return (
-                <div className="square white_square"></div>
-            );
-        } else {
-            return (
-                <div className="square black_square"></div>
-            );
-        }
+function initializeBoard() {
+    const squares = Array(64).fill(null);
+    //black pawns
+    for (let i = 8; i < 16; i++) {
+        squares[i] = (<img src = "https://upload.wikimedia.org/wikipedia/commons/c/c7/Chess_pdt45.svg">
+        </img>);
+    }
+
+    // white pawns
+    for (let i = 8*6; i < 8*6+8; i++) {
+        squares[i] = (<img src = "https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg">
+        </img>);
+    }
+
+    // black knights
+    squares[1] = (<img src = "https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg">
+    </img>);
+    squares[6] = (<img src = "https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg">
+    </img>);
+
+    // white knights
+    squares[56+1] = (<img src = "https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg">
+    </img>);
+    squares[56+6] = (<img src = "https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg">
+    </img>);
+
+    // black bishops
+    squares[2] = (<img src = "https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg">
+    </img>);
+    squares[5] = (<img src = "https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg">
+    </img>);
+
+    // white bishops
+    squares[56+2] = (<img src = "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg">
+    </img>);
+    squares[56+5] = (<img src = "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg">
+    </img>);
+
+    // black rooks
+    squares[0] = (<img src = "https://upload.wikimedia.org/wikipedia/commons/f/ff/Chess_rdt45.svg">
+    </img>);
+    squares[7] = (<img src = "https://upload.wikimedia.org/wikipedia/commons/f/ff/Chess_rdt45.svg">
+    </img>);
+
+    // white rooks
+    squares[56+0] = (<img src = "https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg">
+    </img>);
+    squares[56+7] = (<img src = "https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg">
+    </img>);
+
+    // black queen & king
+    squares[3] = (<img src = "https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg">
+    </img>);
+    squares[4] = (<img src = "https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg">
+    </img>);
+
+    // white queen & king
+    squares[56+3] = (<img src = "https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg">
+    </img>);
+    squares[56+4] = (<img src = "https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg">
+    </img>);
+
+    return squares;
+}
+
+class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: this.props.squares
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = (<img src = "https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg">
+        </img>);
+        this.setState( {
+            squares:squares,
+        });
     }
 
     render() {
+        const board = [];
+        for (let i = 0; i < 8; i++) {
+            const squareRows = [];
+            for (let j = 0; j < 8; j++) {
+                if (i == 0 && j == 0) {
+                    squareRows.push(<Square value = {this.state.squares[(i*8) + j]}
+                        color = "white_square" corner = " top_left_square"
+                        onClick = {() => this.handleClick((i*8) + j)} />
+                    );
+                } else if (i == 0 && j == 7) {
+                    squareRows.push(<Square value = {this.state.squares[(i*8) + j]}
+                        color = "black_square" corner = " top_right_square"
+                        onClick = {() => this.handleClick((i*8) + j)} />
+                    );
+                } else if (i == 7 && j == 0) {
+                    squareRows.push(<Square value = {this.state.squares[(i*8) + j]}
+                        color = "black_square" corner = " bottom_left_square"
+                        onClick = {() => this.handleClick((i*8) + j)} />
+                    );
+                } else if (i == 7 && j ==7) {
+                    squareRows.push(<Square value = {this.state.squares[(i*8) + j]}
+                        color = "white_square" corner = " bottom_right_square"
+                        onClick = {() => this.handleClick((i*8) + j)} />);
+                } else {
+                    const squarecolor = (isEven(i) && isEven(j)) || (!isEven(i) && !isEven(j))? "white_square" : "black_square";
+                    squareRows.push(<Square value = {this.state.squares[(i*8) + j]}
+                        color = {squarecolor} corner = ""
+                        onClick = {() => this.handleClick((i*8) + j)} />
+                    );
+                }
+            }
+            board.push(<div>{squareRows}</div>)
+        }
+
         return (
-            <div className="table">
-                <div>
-                    {this.renderSquare('A8')}
-                    {this.renderSquare('B8')}
-                    {this.renderSquare('C8')}
-                    {this.renderSquare('D8')}
-                    {this.renderSquare('E8')}
-                    {this.renderSquare('F8')}
-                    {this.renderSquare('G8')}
-                    {this.renderSquare('H8')}
-                </div>
-                <div>
-                    {this.renderSquare('A7')}
-                    {this.renderSquare('B7')}
-                    {this.renderSquare('C7')}
-                    {this.renderSquare('D7')}
-                    {this.renderSquare('E7')}
-                    {this.renderSquare('F7')}
-                    {this.renderSquare('G7')}
-                    {this.renderSquare('H7')}
-                </div>
-                <div>
-                    {this.renderSquare('A6')}
-                    {this.renderSquare('B6')}
-                    {this.renderSquare('C6')}
-                    {this.renderSquare('D6')}
-                    {this.renderSquare('E6')}
-                    {this.renderSquare('F6')}
-                    {this.renderSquare('G6')}
-                    {this.renderSquare('H6')}
-                </div>
-                <div>
-                    {this.renderSquare('A5')}
-                    {this.renderSquare('B5')}
-                    {this.renderSquare('C5')}
-                    {this.renderSquare('D5')}
-                    {this.renderSquare('E5')}
-                    {this.renderSquare('F5')}
-                    {this.renderSquare('G5')}
-                    {this.renderSquare('H5')}
-                </div>
-                <div>
-                    {this.renderSquare('A4')}
-                    {this.renderSquare('B4')}
-                    {this.renderSquare('C4')}
-                    {this.renderSquare('D4')}
-                    {this.renderSquare('E4')}
-                    {this.renderSquare('F4')}
-                    {this.renderSquare('G4')}
-                    {this.renderSquare('H4')}
-                </div>
-                <div>
-                    {this.renderSquare('A3')}
-                    {this.renderSquare('B3')}
-                    {this.renderSquare('C3')}
-                    {this.renderSquare('D3')}
-                    {this.renderSquare('E3')}
-                    {this.renderSquare('F3')}
-                    {this.renderSquare('G3')}
-                    {this.renderSquare('H3')}
-                </div>
-                <div>
-                    {this.renderSquare('A2')}
-                    {this.renderSquare('B2')}
-                    {this.renderSquare('C2')}
-                    {this.renderSquare('D2')}
-                    {this.renderSquare('E2')}
-                    {this.renderSquare('F2')}
-                    {this.renderSquare('G2')}
-                    {this.renderSquare('H2')}
-                </div>
-                <div>
-                    {this.renderSquare('A1')}
-                    {this.renderSquare('B1')}
-                    {this.renderSquare('C1')}
-                    {this.renderSquare('D1')}
-                    {this.renderSquare('E1')}
-                    {this.renderSquare('F1')}
-                    {this.renderSquare('G1')}
-                    {this.renderSquare('H1')}
+            <div>
+                <div className="table">
+                    {board}
                 </div>
             </div>
         );
     }
 }
 
+function isEven(value) {
+    return value %2;
+}
+
 class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <Board />
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            squares: initializeBoard(),
+        };
+    }
+    render() {
+        return (
+            <div className="game">
+                <Board squares = {this.state.squares}/>
+            </div>
+        );
+    }
 }
 
 // ========================================
 
 ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
+    <Game />,
+    document.getElementById('root')
 );
