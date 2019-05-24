3cec18579447ad6d2f181e2ef08ccbbbff2078c2
Arpan Sahoo <arpansahooedu@gmail.com>
2019-05-24 19:59:38 -0400

    Color change when piece selected

--- js/chess.js
+++ js/chess.js
@@ -1,6 +1,7 @@
 class King {
     constructor(player) {
         this.player = player;
+        this.underlay = 0;
         this.icon = (player == 'w' ?
             <img src="https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg"></img>
             : <img src="https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg"></img>);
@@ -11,6 +12,7 @@ class King {
 class Queen {
     constructor(player) {
         this.player = player;
+        this.underlay = 0;
         this.icon = (player == 'w' ?
             <img src="https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg"></img>
             : <img src="https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg"></img>);
@@ -21,6 +23,7 @@ class Queen {
 class Knight {
     constructor(player) {
         this.player = player;
+        this.underlay = 0;
         this.icon = (player == 'w' ?
             <img src="https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg"></img>
             : <img src="https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg"></img>);
@@ -31,6 +34,7 @@ class Knight {
 class Bishop {
     constructor(player) {
         this.player = player;
+        this.underlay = 0;
         this.icon = (player == 'w' ?
             <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg"></img>
             : <img src="https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg"></img>);
@@ -41,6 +45,7 @@ class Bishop {
 class Pawn {
     constructor(player) {
         this.player = player;
+        this.underlay = 0;
         this.icon = (player == 'w' ?
             <img src="https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg"></img>
             : <img src="https://upload.wikimedia.org/wikipedia/commons/c/c7/Chess_pdt45.svg"></img>);
@@ -51,6 +56,7 @@ class Pawn {
 class Rook {
     constructor(player) {
         this.player = player;
+        this.underlay = 0;
         this.icon = (player == 'w' ?
             <img src="https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg"></img>
             : <img src="https://upload.wikimedia.org/wikipedia/commons/f/ff/Chess_rdt45.svg"></img>);
@@ -86,11 +92,12 @@ class Board extends React.Component {
 
     handleClick(i) {
         const copy_squares = this.state.squares.slice();
-        console.log("Source#: " + this.state.source);
         if (this.state.source == -1) {
             if (copy_squares[i] != null) {
+                copy_squares[i].underlay = 1;
                 this.setState( {
                     source: i,
+                    squares:copy_squares,
                 });
             }
         }
@@ -99,8 +106,10 @@ class Board extends React.Component {
                 copy_squares[i] = copy_squares[this.state.source];
                 copy_squares[this.state.source] = null;
             }
+            copy_squares[i].underlay = 0;
             this.setState( {
                 source: -1,
+                squares:copy_squares,
             });
         }
         this.setState( {
@@ -108,35 +117,68 @@ class Board extends React.Component {
         });
     }
 
+    // Render the board.
     render() {
         const board = [];
         for (let i = 0; i < 8; i++) {
             const squareRows = [];
             for (let j = 0; j < 8; j++) {
                 if (i == 0 && j == 0) {
-                    squareRows.push(<Square value = {this.state.squares[(i*8) + j]}
-                        color = "white_square" corner = " top_left_square"
+                    const square_color = (this.state.squares[(i*8) + j] == null
+                        || this.state.squares[(i*8) + j].underlay == 0) ? "white_square":"selected_white_square";
+                    squareRows.push(<Square
+                        value = {this.state.squares[(i*8) + j]}
+                        color = {square_color}
+                        corner = " top_left_square"
                         onClick = {() => this.handleClick((i*8) + j)} />
                     );
                 } else if (i == 0 && j == 7) {
-                    squareRows.push(<Square value = {this.state.squares[(i*8) + j]}
-                        color = "black_square" corner = " top_right_square"
+                    const square_color = (this.state.squares[(i*8) + j] == null
+                        || this.state.squares[(i*8) + j].underlay == 0) ? "black_square":"selected_black_square";
+                    squareRows.push(<Square
+                        value = {this.state.squares[(i*8) + j]}
+                        color = {square_color}
+                        corner = " top_right_square"
                         onClick = {() => this.handleClick((i*8) + j)} />
                     );
                 } else if (i == 7 && j == 0) {
-                    squareRows.push(<Square value = {this.state.squares[(i*8) + j]}
-                        color = "black_square" corner = " bottom_left_square"
+                    const square_color = (this.state.squares[(i*8) + j] == null
+                        || this.state.squares[(i*8) + j].underlay == 0) ? "black_square":"selected_black_square";
+                    squareRows.push(<Square
+                        value = {this.state.squares[(i*8) + j]}
+                        color = {square_color}
+                        corner = " bottom_left_square"
                         onClick = {() => this.handleClick((i*8) + j)} />
                     );
                 } else if (i == 7 && j ==7) {
-                    squareRows.push(<Square value = {this.state.squares[(i*8) + j]}
-                        color = "white_square" corner = " bottom_right_square"
-                        onClick = {() => this.handleClick((i*8) + j)} />);
+                    const square_color = (this.state.squares[(i*8) + j] == null
+                        || this.state.squares[(i*8) + j].underlay == 0) ? "white_square":"selected_white_square";
+                    squareRows.push(<Square
+                        value = {this.state.squares[(i*8) + j]}
+                        color = {square_color}
+                        corner = " bottom_right_square"
+                        onClick = {() => this.handleClick((i*8) + j)} />
+                    );
                 } else {
-                    const squarecolor = (isEven(i) && isEven(j)) || (!isEven(i) && !isEven(j))? "white_square" : "black_square";
-                    squareRows.push(<Square value = {this.state.squares[(i*8) + j]}
-                        color = {squarecolor} corner = ""
-                        onClick = {() => this.handleClick((i*8) + j)} />);
+                    const square_color = (isEven(i) && isEven(j)) || (!isEven(i) && !isEven(j))
+                        ? "white_square" : "black_square";
+                    if (this.state.squares[(i*8) + j] == null || this.state.squares[(i*8) + j].underlay == 0) {
+                        squareRows.push(<Square
+                            value = {this.state.squares[(i*8) + j]}
+                            color = {square_color}
+                            corner = ""
+                            onClick = {() => this.handleClick((i*8) + j)} />
+                        );
+                    } else {
+                        const square_color = (isEven(i) && isEven(j)) || (!isEven(i) && !isEven(j))
+                            ? "selected_white_square" : "selected_black_square";
+                        squareRows.push(<Square
+                            value = {this.state.squares[(i*8) + j]}
+                            color = {square_color}
+                            corner = ""
+                            onClick = {() => this.handleClick((i*8) + j)} />
+                        );
+                    }
                 }
             }
             board.push(<div>{squareRows}</div>)
