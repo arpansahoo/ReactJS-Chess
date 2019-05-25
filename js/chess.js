00490dd3f9f5a419812b7cc7708464baa9cb19cd
Arpan Sahoo <arpansahooedu@gmail.com>
2019-05-25 00:04:44 -0400

    Update chess.js

--- js/chess.js
+++ js/chess.js
@@ -182,6 +182,20 @@ class Board extends React.Component {
         };
     }
 
+    check_blockers(start, end) {
+        var start_row = 8 - Math.floor(start / 8);
+        var start_col = start % 8 + 1;
+        var end_row = 8 - Math.floor(end / 8);
+        var end_col = end % 8 + 1;
+
+        var row_diff = end_row - start_row;
+        var col_diff = end_col - start_col;
+
+        if (col_diff == 0) {
+
+        }
+    }
+
     handleClick(i) {
         const copy_squares = this.state.squares.slice();
 
